c9dd7a02f7782396d519e90688d0f95721602060
MrRonnie <ronymaza55@gmail.com>
2022-04-01 23:24:04 +0600

    makeing order chart and other stuff

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import useCart from '../../hooks/useCart';
+import useProducts from '../../hooks/UseProducts';
+import Cart from '../Cart/Cart';
+import ReviewItem from '../ReviewItem/ReviewItem';
+
 
 const Orders = () => {
+    const [ products, setProducts ] = useProducts();
+    const [cart, setCart] = useCart(products);
     return (
-        <div>
-            <h2>
-                This is Orders!
-            </h2>
+        <div className='shop-container'>
+            <div className="review-items-container">
+                {
+                    cart.map(product => <ReviewItem key={product.id}
+                    product = {product}>
+
+                    </ReviewItem>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}></Cart>
+            </div>
         </div>
     );
 };
